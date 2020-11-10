768e7035c9b0cff3bdc29fcfb3dbe3a964de9994
taxijjang <gw9122@naver.com>
2020-11-11 02:24:57 +0900

    fix/api수정

--- product/views/product_views.py
+++ product/views/product_views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework.status import HTTP_404_NOT_FOUND
+from rest_framework import status
+from django.http import Http404
 from ..serializers.product_serializers import ProductSerializer
 
 from ..models import Product
@@ -16,14 +17,10 @@ class ProductViewSet(ModelViewSet):
         return Response(data=serializer.data)
 
     def retrieve(self, request, *args, **kwargs):
-        try:
-            pk = kwargs.pop('pk')
-            instance = self.get_queryset().filter(pk=pk).prefetch_related('tag_set').prefetch_related('option_set')[0]
-            serializer = self.get_serializer(instance)
-            return Response(data=serializer.data)
-
-        except IndexError:
-            return Response(HTTP_404_NOT_FOUND)
+        pk = kwargs.pop('pk')
+        instance = self.get_queryset().filter(pk=pk).prefetch_related('tag_set').prefetch_related('option_set').first()
+        serializer = self.get_serializer(instance)
+        return Response(data=serializer.data)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -38,3 +35,8 @@ class ProductViewSet(ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(data=serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
