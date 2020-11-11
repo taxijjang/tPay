d5e768a5855c8efcb551e54896bbf73d9f605e57
taxijjang <gw9122@naver.com>
2020-11-12 04:38:49 +0900

    feat/#4/pytest설정

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,9 +2,6 @@ from .base import *
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
