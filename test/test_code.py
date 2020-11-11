07b9f840d59f90f740399473a147988908cfb11c
taxijjang <gw9122@naver.com>
2020-11-12 08:31:53 +0900

    feat/#4/unit-test 완료

--- test/test_code.py
+++ test/test_code.py
@@ -1,31 +1,108 @@
 import pytest
 import json
-from rest_framework.test import APITestCase
+from product.models import Tag, Product, ProductOption
+from rest_framework.test import APIClient, APITestCase
 from product.serializers.product_serializers import ProductSerializer
 
 
 class TestView(APITestCase):
-    ## 비어있는 product를 요청
+    def setUp(self):
+        '''
+            1. 초기 product 더미 생성
+        '''
+        self.client = APIClient()
+
+        request_data = {
+            "name": "unittest_product_1",
+            "option_set": [
+                {
+                    "name": "unittest_option_1",
+                    "price": 1111
+                },
+                {
+                    "name": "unittest_option_2",
+                    "price": 2222
+                }
+            ],
+            "tag_set": [
+                {
+                    "name": "unittest_tag_1"
+                },
+                {
+                    "name": "unittest_tag_2"
+                }
+            ]
+        }
+
+        self.products = [{
+            "pk": 1,
+            "name": "unittest_product_1",
+            "option_set": [
+                {
+                    "pk": 1,
+                    "name": "unittest_option_1",
+                    "price": 1111
+                },
+                {
+                    "pk": 2,
+                    "name": "unittest_option_2",
+                    "price": 2222
+                }
+            ],
+            "tag_set": [
+                {
+                    "pk": 1,
+                    "name": "unittest_tag_1"
+                },
+                {
+                    "pk": 2,
+                    "name": "unittest_tag_2"
+                }
+            ]
+        }]
+
+        url = "/shop/products/"
+        self.client.post(
+            url, json.dumps(request_data), content_type='application/json'
+        )
+
     def test_get_product(self):
         '''
-            1. 비어있는 products list를 요청 - GET
-            2. 비어있는 prodcuts detail을 요청 - GET
+            1. products list를 요청 - GET
+            2. prodcuts detail을 요청 - GET
+            3. 비어있는 products detail을 요청 - GET
         '''
-        url = "/shop/products/"
-        res = self.client.get(
-            url,
-            {},
-            content_type="application/json"
-        )
-        assert res.status_code == 200
-        assert res.data == []
 
-        ## 비어있는 products detail을 요청 했을때
-        url = "/shop/products/1"
+        ## 1번
+        url = "/shop/products/"
         res = self.client.get(
             url, {}, content_type="application/json"
         )
-        assert res.status_code == 301
+        assert res.status_code == 200
+        assert json.dumps(res.data) == json.dumps(self.products)
+
+        ## 2번
+        url = "/shop/products/1/"
+        res = self.client.get(
+            url, {}, content_type="application/json"
+        )
+
+        assert res.status_code == 200
+        assert json.dumps(res.data) == json.dumps(self.products[0])
+
+        ## 3번
+        url = "/shop/products/2/"
+        res = self.client.get(
+            url, {}, content_type="application/json"
+        )
+
+        check_data = {
+            'name': '',
+            'option_set': [],
+            'tag_set': []
+        }
+        assert res.status_code == 200
+        assert res.data == check_data
 
     def test_post_product(self):
         '''
@@ -38,38 +115,34 @@ class TestView(APITestCase):
         url = "/shop/products/"
 
         request_data = {
-            "name": "unittest",
-            "option_set": [
-                {
-                    "name": "unittest_option",
-                    "price": 777
-                }
-            ],
-            "tag_set": [
-                {
-                    "name": "unittest_tag"
-                }
-            ]
-        }
-
-        check_data = {
-            "pk": 1,
-            "name": "unittest",
+            "name": "unittest_product_2",
             "option_set": [
                 {
                     "pk": 1,
-                    "name": "unittest_option",
-                    "price": 777
+                    "name": "unittest_option_1",
+                    "price": 1111
+                },
+                {
+                    "name": "unittest_option_3",
+                    "price": 2222
                 }
             ],
             "tag_set": [
                 {
                     "pk": 1,
-                    "name": "unittest_tag"
+                    "name": "unittest_tag_1"
+                },
+                {
+                    "name": "unittest_tag_3"
                 }
             ]
         }
 
+        check_data = {"pk": 2}
+        check_data.update(request_data)
+        check_data['option_set'][1] = {"pk": 3, "name": "unittest_option_3", "price": 3333}
+        check_data['tag_set'][1] = {"pk": 3, "name": "unit_test_tag_3"}
+        self.products.append(check_data)
 
         res = self.client.post(
             url,
@@ -77,7 +150,7 @@ class TestView(APITestCase):
             content_type='application/json'
         )
         assert res.status_code == 201
-        assert res.data == check_data
+        assert json.dumps(res.data) == json.dumps(check_data)
 
         ## 2번
         url = '/shop/products/'
@@ -85,18 +158,103 @@ class TestView(APITestCase):
             url, {}, content_type="application/json"
         )
 
-        products_data = [check_data]
+        self.products[0]["option_set"].pop(0)
         assert res.status_code == 200
-        assert json.dumps(res.data) == json.dumps(products_data)
+        assert json.dumps(res.data) == json.dumps(self.products)
 
         ## 3번
-        url = '/shop/products/1/'
+        url = '/shop/products/2/'
         res = self.client.get(
             url, {}, content_type="application/json"
         )
 
         assert res.status_code == 200
-        assert json.dumps(res.data) == json.dumps(products_data[0])
+        assert json.dumps(res.data) == json.dumps(check_data)
 
     def test_patch_product(self):
-        pass
+        '''
+            1. 기존의 1번 product 수정 - PATCH
+            2. 수정된 parouct list 조회 - GET
+            3. 수정된 product detail 조회 - GET
+        '''
+
+        ## 1번
+        url = '/shop/products/1/'
+        request_data = {
+            "pk": 1,
+            "name": "unittest_product_1",
+            "option_set": [
+                {
+                    "pk": 1,
+                    "name": "unittest_option_1",
+                    "price": 1111
+                },
+                {
+                    "name": "unittest_option_3",
+                    "price": 2222
+                }
+            ],
+            "tag_set": [
+                {
+                    "pk": 1,
+                    "name": "unittest_tag_1"
+                },
+                {
+                    "name": "unittest_tag_3"
+                }
+            ]
+        }
+
+        check_data = {
+            "pk": 1,
+            "name": "unittest_product_1",
+            "option_set": [
+                {
+                    "pk": 1,
+                    "name": "unittest_option_1",
+                    "price": 1111
+                },
+                {
+                    "pk": 3,
+                    "name": "unittest_option_3",
+                    "price": 2222
+                }
+            ],
+            "tag_set": [
+                {
+                    "pk": 1,
+                    "name": "unittest_tag_1"
+                },
+                {
+                    "pk": 3,
+                    "name": "unittest_tag_3"
+                }
+            ]
+        }
+        res = self.client.patch(
+            url, data=json.dumps(request_data), content_type="application/json"
+        )
+        print(res.data)
+
+        assert res.status_code == 200
+        assert json.dumps(res.data) == json.dumps(check_data)
+
+        ## 2번
+
+        url = '/shop/products/'
+        res = self.client.get(
+            url, data={}, content_type="application/json"
+        )
+
+        assert res.status_code == 200
+        assert json.dumps(res.data) == json.dumps([check_data])
+
+        ## 3번
+
+        url = '/shop/products/1/'
+        res = self.client.get(
+            url, data={}, content_type="application/json"
+        )
+
+        assert res.status_code == 200
+        assert json.dumps(res.data) == json.dumps(check_data)
